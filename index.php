241fd753b9c2ec8e28329b2ff6420ebba3d61445
Vinicius3Ritz <vinicius.ritz@alunos.fibbauru.br>
2024-11-06 22:06:42 -0300

    Iniciando

---
+++ index.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\styles.css">
+    <title>Loja da Nintendo</title>
+</head>
+<body>
+    <header>
+        <h1>Nintendo e-Shop</h1>
+    </header>
+    <main>
+        <section id="intro">
+            <h3>História da Nintendo</h3>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Os animes sãA Nintendo Co. Ltd., sediada em Quioto, no Japão, é responsável por séries que já se tornaram ícones em todo o mundo, através dos seus produtos de hardware e software integrados. Mario™, Donkey Kong™, The Legend of Zelda™, Metroid™, Pokémon™, Animal Crossing™, Pikmin™ e Splatoon™ são apenas algumas das mais populares neste momento.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A Nintendo tem como objetivo proporcionar a todos experiências de entretenimento únicas e intuitivas, fabricando e comercializando dispositivos de videojogos como a família de consolas Nintendo Switch™, desenvolvendo e operando aplicações para dispositivos móveis e colaborando com parceiros numa variedade de outras iniciativas de entretenimento, como conteúdos visuais e parques temáticos.
+            </p>
+        </section>
+        <section id="anime-list">
+            <h2>Jogos Populares</h2>
+            <table>
+                <tr>
+                    <td><img src="image/jogo1.jpg" ></td>
+                    <td><img src="image/jogo2.jpg" ></td>
+                    <td><img src="image/jogo3.png" ></td>
+                </tr>
+            </table>
+        </section>
+        <section id="outro">
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A Nintendo já vendeu mais de 5.5 mil milhões de videojogos e mais de 800 milhões de consolas em todo o mundo. Desde o lançamento da Nintendo Entertainment System™ há mais de 30 anos e até ao futuro, a missão contínua da Nintendo é a criação de experiências de entretenimento únicas capazes de colocar sorrisos nos rostos de pessoas em todo o mundo.
+            </p>
+        </section>
+    </main>
+    <footer>
+        <a href="php/etapa2.php">Comprar</a>
+    </footer>
+</body>
+</html>
